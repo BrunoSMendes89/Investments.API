af3648e9c12b44d4ba70144e6d3379c4243d82cd
Bruno Mendes <102331277+BrunoSMendes89@users.noreply.github.com>
2024-06-27 20:48:00 -0300

    Base Api is Done

---
+++ Investments.Api/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Investments.Api.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("HelloWorld")]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
