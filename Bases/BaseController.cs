a8fe63078056a6721c4b5a4585513b9d1724ef74
Bruno Mendes <102331277+BrunoSMendes89@users.noreply.github.com>
2024-06-27 21:47:01 -0300

    MediatR configuration done.

---
+++ Bases/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Bases
+{
+    public abstract class BaseController<TController> : ControllerBase
+    {
+        protected readonly IMediator _mediator;
+
+        protected BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        protected async Task<IActionResult> CreateActionResult<T>(T model)
+        {
+            try
+            {
+                if (model == null)
+                    throw new NotImplementedException();
+                var result = await _mediator.Send(model);
+                if (result is FileStreamResult fileStream)
+                    return fileStream;
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(ex)
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+        }
+
+    }
+}
---
+++ Service/Models/TestResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Service.Models
+{
+    public class TestResponse
+    {
+        public string Response { get; set; }
+    }
+}
