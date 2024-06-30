4b29bb63615e45e1d409456cd60bdff12d34f05d
Bruno Mendes <102331277+BrunoSMendes89@users.noreply.github.com>
2024-06-30 12:47:08 -0300

    Get products done.

---
+++ Service/Models/GetProductsRequest.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+using MediatR;
+
+namespace Service.Models
+{
+    public class GetProductsRequest : IRequest<List<GetProductsResponse>>
+    {
+        public int? Id { get; set; }
+        public string Description { get; set; }
+        public ProductTypeEnum? ProductType { get; set; }
+    }
+}
---
+++ Service/Models/GetProductsResponse.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Service.Models
+{
+    public class GetProductsResponse
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public ProductTypeEnum ProductType { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
